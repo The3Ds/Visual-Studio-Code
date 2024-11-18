614dfb672b15d16bcb92336a9710104c2185af4e
The3Ds <135562088+The3Ds@users.noreply.github.com>
2024-11-17 18:41:57 -0600

    Por resolver

---
+++ Python (.py)/Ejercicios de Programación/Eje07.py
@@ -0,0 +1,15 @@
+# TODO Ejericio 7 de lógica
+
+"""
+
+/*
+ * Crea un programa que invierta el orden de una cadena de texto
+ * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ */
+
+"""
+
+
+
+
